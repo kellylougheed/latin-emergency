245a42ca252b361d6e0e6edd516958f005f599a1
Kelly Lougheed <kellylougheed@gmail.com>
2016-08-30 15:42:27 +0000

    Made the form functional

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -6,5 +6,16 @@ class ActivitiesController < ApplicationController
   def new
     @activity = Activity.new
   end
+
+  def create
+    Activity.create(activity_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:exercise, :level)
+  end
   
 end
